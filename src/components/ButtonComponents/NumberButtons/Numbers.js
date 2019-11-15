e6b18be2bf0f4e2439d1094ee05355d3e9399316
sametweb <sametmutevellioglu@gmail.com>
2019-11-14 21:23:32 -0800

    Styling finished, numbers functionality added

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,19 +1,25 @@
 import React from "react";
+import NumberButton from "./NumberButton";
 
 //import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
+// example of import from data.js. Note all the ../   This is how we move through folders.
 /* 
 import { numbers } from '../../../data' 
 */
 //Import your array data to from the provided data file
 
-const Numbers = () => {
+const Numbers = ({ numbers, getValue }) => {
   // STEP 2 - add the imported data to state
   return (
-    <div>
+    <div className="numbers">
+      {numbers.map((item, index) => (
+        <NumberButton key={index} value={item} getValue={getValue} />
+      ))}
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
     </div>
   );
 };
+
+export default Numbers;
--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = ({ value }) => {
   return (
-    <>
+    <button>
+      {value}
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    </button>
   );
 };
+
+export default OperatorButton;
